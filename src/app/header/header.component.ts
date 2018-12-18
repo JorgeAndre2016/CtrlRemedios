abffe31a0d66b781783fcbed032e37cb32ada2a1
Jorge André <jorge_aquino@hotmail.com.br>
2018-12-18 01:51:33 -0200

    Adicionando opção de login e logout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../servicos/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'rem-header',
@@ -6,4 +8,24 @@ import { Component } from '@angular/core';
     styleUrls: ['./header.component.css']
 })
 
-export class HeaderComponent {}
+export class HeaderComponent {
+
+    public autenticado: boolean;
+
+    constructor(
+        private authService: AuthService,
+        private router: Router
+    ) {
+        this.authService.authUser()
+            .subscribe((r) => {
+                this.autenticado = true;
+            });
+    }
+
+    public signOut(): void {
+        this.authService.logout()
+            .then((resolve) => {
+                this.router.navigate(['']);
+            });
+    }
+}
